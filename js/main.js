8baccc7f9dddd94885facf46c7ce9063c39e4648
SergSvet87 <94105673+SergSvet87@users.noreply.github.com>
2022-06-08 08:36:03 +0300

    My_RESUME

--- js/main.js
+++ js/main.js
@@ -33,12 +33,12 @@ function changeLang() {
 
 function changeLangBg() {
   if (select.value === allLang[1]) {
-    header.style.background = "url(../img/flag-of-ua.webp) center / cover no-repeat"
+    header.style.background = "url(../img/flag-of-ua.jpg) center / cover no-repeat"
   } else if (select.value === allLang[0]) {
-    header.style.background = "url(../img/flag-of-en.webp) no-repeat";
+    header.style.background = "url(../img/flag-of-en.jpg) no-repeat";
     header.style.backgroundSize = "100% 100%";
   } else if (select.value === allLang[2]) {
-    header.style.background = "url(../img/flag-of-ru.webp) 50% / cover no-repeat";
+    header.style.background = "url(../img/flag-of-ru.jpg) 50% / cover no-repeat";
     header.style.backgroundSize = "100% 100%";
   }
 }
